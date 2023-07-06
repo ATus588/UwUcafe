cf12910e6d1695361432bf01f742281a2f07e36c
Anh_Tu Tran <anhtutran885@gmail.com>
2023-07-06 11:07:34 +0700

    list store

--- src/pages/CreateStore.js
+++ src/pages/CreateStore.js
@@ -4,7 +4,8 @@ import penImg from '../images/pen.png'
 import cameraImg from '../images/camera.png'
 import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify';
 import apiClient from '../APIclient'
 import ShowStar from '../components/showStar'
 
@@ -19,7 +20,7 @@ const Address = ({ address }) => {
 
 
 function CreateStore() {
-
+    const navigate = useNavigate()
     const { t } = useTranslation()
     const { restaurantId } = useParams()
 
@@ -59,7 +60,6 @@ function CreateStore() {
 
     useEffect(() => {
         getData();
-        console.log(store)
         const userJson = localStorage.getItem('user');
         const userP = userJson ? JSON.parse(userJson) : null;
         setUser(userP);
@@ -76,7 +76,18 @@ function CreateStore() {
         const { checked, value } = event.target
         let newServices = store.services
         if (checked) {
-            newServices.push(value)
+            if (value == '1') {
+                newServices.push(1)
+            }
+            if (value == '2') {
+                newServices.push(2)
+            }
+            if (value == '3') {
+                newServices.push(3)
+            }
+            if (value == '4') {
+                newServices.push(4)
+            }
             setStore({ ...store, services: newServices })
         } else {
             var index = newServices.indexOf(value);
@@ -92,74 +103,85 @@ function CreateStore() {
         setStore({ ...store, items: newMenu })
         setNewItem({ name: '', price: '', description: '' })
     }
-    const removeItem = (item) => {
-        let newMenu = store.items.filter(function (i) {
-            return i !== item
-        })
-        setStore({ ...store, items: newMenu })
+    const onChangeCrTime = (event) => {
+        setStore({ ...store, crowded_time: event.target.value })
     }
-
-    function onSubmit(event) {
-        console.log(store);
-
+    const onChangeCrTimeEnd = (event) => {
+        setStore({ ...store, end_crowded_time: event.target.value })
     }
+    // const removeItem = (item) => {
+    //     let newMenu = store.items.filter(function (i) {
+    //         return i !== item
+    //     })
+    //     setStore({ ...store, items: newMenu })
+    // }
+
+    const handleSave = async () => {
+        console.log(store)
+        try {
+            const response = await apiClient.put(`/update_store`, { store })
+            if (response.status === 200) {
+                toast('Update success!')
+                navigate(0)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    };
 
     return (
         <>
-            <div class="flex-container content_area">
-                <div class="cafe_image_container">
-                    {store.logo && <img class="image_change" src={store.logo} alt="" />}
+            <div className="flex-container content_area">
+                <div className="cafe_image_container">
+                    {store.logo && <img className="image_change" src={store.logo} alt="" />}
                     <input id="image_input" type="file" hidden />
-                    <label for="image_input" class="flex-container align-content-center camera_button">
-                        <img class="camera_icon" src={cameraImg} alt="" />
-                        <div class="camera_text">
+                    <label htmlFor="image_input" className="flex-container align-content-center camera_button">
+                        <img className="camera_icon" src={cameraImg} alt="" />
+                        <div className="camera_text">
                             Thay đổi hình ảnh
                         </div>
                     </label>
-                    <div className='save-btn' onClick={() => onSubmit()}>Save</div>
+                    <div className='save-btn' onClick={handleSave}>Save</div>
                 </div>
 
-                <div class="divine-line">
+                <div className="divine-line">
                 </div>
 
-                <div class="cafe_info_container">
-                    <div class="flex-container space-between">
+                <div className="cafe_info_container">
+                    <div className="flex-container space-between">
                         {changeName ? <input className='store-name-input' onChange={onChangeName} /> : <div className='cafe_name'>{store.name}</div>}
 
                         <img src={penImg} style={{ height: '50px', alignSelf: "flex-end" }} alt="" onClick={() => setChangeName(!changeName)} />
                     </div>
-                    <div class="flex-container space-between">
+                    <div className="flex-container space-between">
                         {changeName ? <input className='store-address-input' onChange={onChangeAddress} /> : <Address address={store.address} />}
                     </div>
                     {store.total_star && <ShowStar star={store.total_star} total_review={store.view} />}
-                    <div class="flex-container space-between">
-                        <div class="title">
+                    <div className="flex-container space-between">
+                        <div className="title">
                             Menu
                         </div>
                         <img src={penImg} style={{ height: '50px', alignSelf: "flex-end" }} alt="" />
                     </div>
 
-                    <div class="divine-line-horizontal">
+                    <div className="divine-line-horizontal">
                     </div>
 
-                    <div class="menu_container">
-                        <div class="menu_input_item flex-container space-between">
-                            <input id="drink_input" class="custom_input_store" type="text" onChange={(event) => setNewItem({ ...newItem, name: event.target.value })} />
-                            <input id="price_input" class="custom_input_store price_input" type="number" onChange={(event) => setNewItem({ ...newItem, price: event.target.value })} />
+                    <div className="menu_container">
+                        <div className="menu_input_item flex-container space-between">
+                            <input id="drink_input" className="custom_input_store" type="text" onChange={(event) => setNewItem({ ...newItem, name: event.target.value })} />
+                            <input id="price_input" className="custom_input_store price_input" type="number" onChange={(event) => setNewItem({ ...newItem, price: event.target.value })} />
                             <button className='' onClick={handleAddItem}>Add</button>
                         </div>
                         {
                             store.items && store.items.map((item, index) => {
                                 return (
-                                    <div class="flex-container space-between menu_item" key={index}>
-                                        <div class="drink">
+                                    <div className="flex-container space-between menu_item" key={index}>
+                                        <div className="drink">
                                             {item.name}
                                         </div>
-                                        <div class="price">
+                                        <div className="price">
                                             {item.price}
-                                        </div>
-                                        <div onClick={removeItem(item)}>
-                                            remove
                                         </div>
                                     </div>
                                 )
@@ -167,55 +189,55 @@ function CreateStore() {
                         }
                     </div>
 
-                    <div class="flex-container space-between">
-                        <div class="title">
+                    <div className="flex-container space-between">
+                        <div className="title">
                             Tuỳ chọn
                         </div>
                         <img src={penImg} style={{ height: '50px', alignSelf: "flex-end" }} alt="" />
                     </div>
 
-                    <div class="divine-line-horizontal">
+                    <div className="divine-line-horizontal">
                     </div>
 
-                    <div class="option_container">
-                        <label class="container">
-                            <div class="option_text">
+                    <div className="option_container">
+                        <label className="container">
+                            <div className="option_text">
                                 {t('restaurant.service1')}
                             </div>
                             <input type="checkbox" value={1} onChange={onChangeCheckbox} />
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
-                        <label class="container">
-                            <div class="option_text">
+                        <label className="container">
+                            <div className="option_text">
                                 {t('restaurant.service2')}
                             </div>
                             <input type="checkbox" value={2} onChange={onChangeCheckbox} />
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
-                        <label class="container">
-                            <div class="option_text">
+                        <label className="container">
+                            <div className="option_text">
                                 {t('restaurant.service3')}
                             </div>
                             <input type="checkbox" value={3} onChange={onChangeCheckbox} />
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
-                        <label class="container">
-                            <div class="option_text">
+                        <label className="container">
+                            <div className="option_text">
                                 {t('restaurant.service4')}
                             </div>
                             <input type="checkbox" value={4} onChange={onChangeCheckbox} />
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                     </div>
 
-                    <div class="flex-container align-content-center">
-                        <div class="time">
+                    <div className="flex-container align-content-center">
+                        <div className="time">
                             Khung giờ đông khách
                         </div>
                         {
                             store.crowded_time && <>
-                                <input type="text" class="custom_input_store time_input" value={store.crowded_time} />
-                                <input type="text" class="custom_input_store time_input" value={store.crowded_time_end} />
+                                <input type="text" className="custom_input_store time_input" value={store.crowded_time} onChange={onChangeCrTime} />
+                                <input type="text" className="custom_input_store time_input" value={store.crowded_time_end} onChange={onChangeCrTimeEnd} />
                             </>
                         }
                     </div>
--- src/pages/ListStore.js
+++ src/pages/ListStore.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import '../styles/listStore.css'
+import { useTranslation } from 'react-i18next'
+import apiClient from '../APIclient'
+import RestaurantCard from '../components/RestaurantCard'
+import { Link } from 'react-router-dom'
 
 function ListStore() {
+
+    const [restaurants, setRestaurants] = useState([]);
+
+    async function getRestaurants() {
+        try {
+            const response = await apiClient.get('/owned_store');
+            setRestaurants(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        getRestaurants();
+    }, [])
     return (
-        <div>ListStore</div>
+        <div className='list-store-container'>
+            <h1 className='list-store-title'>Danh sách cửa hàng</h1>
+            <div className="list-store-result">
+                {
+                    restaurants.map((restaurant) => {
+                        return <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+                    })
+                }
+            </div>
+
+            <Link to='./restaurant_new' style={{ textDecoration: 'none', color: 'black' }}><div className='create-new-store-btn'>Thêm cửa hàng mới</div></Link>
+        </div>
     )
 }
 
